c1b406e1838655653a7d183c239228b58410c031
Jefferson <1626966@senati.pe>
2025-10-16 08:18:45 -0500

    comandostxt para recordar

--- src/main/java/com/senati/senati_web_JeffersonTrujillo/controller/UserController.java
+++ src/main/java/com/senati/senati_web_JeffersonTrujillo/controller/UserController.java
@@ -1,4 +1,5 @@
 package com.senati.senati_web_JeffersonTrujillo.controller;
+import com.senati.senati_web_JeffersonTrujillo.model.Response;
 import com.senati.senati_web_JeffersonTrujillo.service.UserService;
 import com.senati.senati_web_JeffersonTrujillo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,13 @@ public class UserController {
         return userService.newUser(user);
     }
     @DeleteMapping ("/api/users/{id}")
-    public ResponseEntity<User> deleteUser(@PathVariable int id) {
-        userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
+    public Response deleteUser(@PathVariable int id){
+        return  userService.deleteUser(id);
     }
+    //public ResponseEntity<User> deleteUser(@PathVariable int id) {
+       // userService.deleteUser(id);
+       // return ResponseEntity.noContent().build();
+    //}
 
     @PutMapping ("/api/users/{id}")
     public ResponseEntity<User> newUser(@PathVariable int id,@RequestBody User user){
